9c0e4660450c72f288cbf97cd68b87bda12de7e3
nerdbow <108768934+nerdbow@users.noreply.github.com>
2025-09-09 08:41:00 -0400

    Add checking functions for env variables for argon2, sqlite db file, api address

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,71 @@ import (
 	"log"
 	"log/slog"
 	"os"
+	"strconv"
 )
 
 func main() {
+	// If no fatal is thrown then all env vars are not empty.
+	checkSQLiteEnv()
+	checkArgonEnv()
+	checkHTTPEnv()
 	initilizeLogging()
+}
+
+func checkSQLiteEnv() {
+	dbfile := os.Getenv("DBFILE")
+	if dbfile == "" {
+		log.Fatalf("Unable to get \"DBFILE\" environmnet variable.\nPlease make sure to set it before starting the API.")
+	}
+	_, err := os.Stat(dbfile)
+	if os.IsNotExist(err) {
+		log.Fatalf("File %s could not be found.\nPlease specify a .db file to use for the database of the API.", dbfile)
+	}
+}
+
+func checkArgonEnv() {
+	time := os.Getenv("ARGON_TIME")
+	if time == "" {
+		log.Fatalf("Unable to get \"ARGON_TIME\" environmnet variable.\nPlease make sure to set it before starting the API.")
+	}
+	n, err := strconv.Atoi(time)
+	if err != nil || n <= 0 {
+		log.Fatalf("Variable \"ARGON_TIME\" must be a positive integer greater than 0.")
+	}
+
+	memory := os.Getenv("ARGON_MEMORY")
+	if memory == "" {
+		log.Fatalf("Unable to get \"ARGON_MEMORY\" environmnet variable.\nPlease make sure to set it before starting the API.")
+	}
+	n, err = strconv.Atoi(memory)
+	if err != nil || n <= 0 {
+		log.Fatalf("Variable \"ARGON_MEMORY\" must be a positive integer greater than 0.")
+	}
+
+	threads := os.Getenv("ARGON_THREADS")
+	if threads == "" {
+		log.Fatalf("Unable to get \"ARGON_THREADS\" environmnet variable.\nPlease make sure to set it before starting the API.")
+	}
+	n, err = strconv.Atoi(threads)
+	if err != nil || n <= 0 {
+		log.Fatalf("Variable \"ARGON_THREADS\" must be a positive integer greater than 0.")
+	}
+
+	hashLength := os.Getenv("ARGON_HASH_LENGTH")
+	if hashLength == "" {
+		log.Fatalf("Unable to get \"ARGON_HASH_LENGTH\" environmnet variable.\nPlease make sure to set it before starting the API.")
+	}
+	n, err = strconv.Atoi(hashLength)
+	if err != nil || n <= 0 {
+		log.Fatalf("Variable \"ARGON_HASH_LENGTH\" must be a positive integer greater than 0.")
+	}
+}
+
+func checkHTTPEnv() {
+	addr := os.Getenv("ADDRESS")
+	if addr == "" {
+		log.Fatalf("Unable to get \"ADDRESS\" environmnet variable.\nPlease make sure to set it before starting the API.")
+	}
 }
 
 func initilizeLogging() {
