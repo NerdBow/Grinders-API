ba019a038b712ef33f2a51a17ab0cd6e57099ca0
nerdbow <108768934+nerdbow@users.noreply.github.com>
2025-09-21 22:45:48 -0400

    Fix documentation for method

--- internal/database/database.go
+++ internal/database/database.go
@@ -6,7 +6,7 @@ type SessionsDB interface{
 	// AddSession will inserts the given session information into the database.
 	AddSession(session util.Session) error
 	// GetSession will query a session with the specified hashedId and userId.
-	// If there is no session then an error will be return.
+	// If there is no session then an empty Session will be returned.
 	GetSession(hashedId string, userId uint64) (util.Session, error)
 	// DeleteSesssion will delete a session specified by the given hashedId.
 	// If unable to delete the session an error will be returned.
